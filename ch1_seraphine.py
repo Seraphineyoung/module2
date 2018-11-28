5aa1482a6fbae719483c9c350930d32528cca68c
Seraphine Young <seraphine_ene@yahoo.com>
2018-11-28 15:58:42 +0000

    first commit

---
+++ ch1_seraphine.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+G = 2 + 3, 5,4,6
+B = 2.9 + 3.2
+C =(3.2 / 2.0) + 7
+D = 2**3
+E = 4**(1/2)
+F = str(6) + 'a'
+A =[1,2,3,4,5]
+J = float(2.5)
+P = int(J)
+O = str(P)
+S = "my name is seraphine"
+
+print(S)
+
+
+
+print(A,B,C,D,E,F)
+
+V = "my name is seraphine"
+
+
+
