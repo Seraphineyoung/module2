732c56af02527df455371fa5e0ac5700d7e223b5
Seraphine Young <seraphine_ene@yahoo.com>
2018-11-28 16:56:41 +0000

    Modified the file

--- ch01/ch1_seraphine.py
+++ ch01/ch1_seraphine.py
@@ -18,6 +18,8 @@ S = "mynameisseraphine"
 print(S)
 print(A,B,C,D,E,F)
 V = "my name is Pamela"
+M = "my name is Pamela" + V
+print(M)
 #In Python everything is an object...
 #This is me learning how to comment on python or crtl A
 
