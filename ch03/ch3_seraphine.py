3a1d0c3e5e94561d917874f42b0ca63cb2600039
Seraphine Young <seraphine_ene@yahoo.com>
2018-11-29 15:32:36 +0000

    Working with function

--- ch03/ch3_seraphine.py
+++ ch03/ch3_seraphine.py
@@ -1,3 +1,5 @@
+#first way
+
 #print('what is your name? ')
 #user_name = input().upper()
 #print('Hello {}!'.format(user_name))
@@ -12,11 +14,29 @@
 #
 #print('Hello {}! you are from {}'.format(user_name,user_country.title()))
 
-name = input("What’s your name? ")
-city = input("What’s your city? ")
-age = input("What’s your age? ")
-age = str(age)
-print ("Hello {}! from {} I am {} years old!".format(name.upper(),city.title(),age ))
+#third way 
+#name = input("What’s your name? ")
+#city = input("What’s your city? ")
+#age = input("What’s your age? ")
+#age = str(age)
+#print ("Hello {}! from {} I am {} years old!".format(name.upper(),city.title(),age ))
 
 
+def hello_world():
+    my_details()
+
+def my_details():
+    user_name = input('What is our name? ')
+    print ("My name is {}".format(user_name.title()))
+    add_numbers()
+    
+def add_numbers():
+    user_age = input('what is your age ? ')
+    print(int(user_age) + 2)
+    favourite_player()
+    
+def favourite_player():
+    input('who is your favourite player? ')
+    print("Your favourite player is Murray yayaya")
+    
 
