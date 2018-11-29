97239e21c9ceaa9621a3057ad334ace4287fb93b
Seraphine Young <seraphine_ene@yahoo.com>
2018-11-29 14:36:11 +0000

    Working on input function

---
+++ ch03/ch3_seraphine.py
@@ -0,0 +1,22 @@
+#print('what is your name? ')
+#user_name = input().upper()
+#print('Hello {}!'.format(user_name))
+
+
+#second way
+
+#user_name = input('What is your name? ')
+#user_country = input('Where are you from? ')
+#
+#user_name = user_name.upper()
+#
+#print('Hello {}! you are from {}'.format(user_name,user_country.title()))
+
+name = input("What’s your name? ")
+city = input("What’s your city? ")
+age = input("What’s your age? ")
+age = str(age)
+print ("Hello {}! from {} I am {} years old!".format(name.upper(),city.title(),age ))
+
+
+
