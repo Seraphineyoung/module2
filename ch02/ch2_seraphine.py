3e5189947a964af6237047c74db4e6628f722b18
Seraphine Young <seraphine_ene@yahoo.com>
2018-11-29 10:19:45 +0000

    Added ch02

---
+++ ch02/ch2_seraphine.py
@@ -0,0 +1,26 @@
+print (5-6)
+8*9
+6/2
+5/2
+5.0/2
+5%2
+2*(10+2)
+print (2**4)
+
+age =5
+age = "almost three"
+age = 2.5
+age= 'I really don\'t know'
+print (type(age))
+
+print('hello' + 'world')
+print('Joke' * 3)
+
+
+late = 'Joke i am late'
+print(('late  \n')  * 10)
+print('chen' + str(3))
+print('hello'.upper())
+print('GOODBYE'.lower())
+print('\n')
+print('the lord of the rings'.title())
