d8bde50043266d58e30d33a5c91c6bd5ef457e74
Seraphine Young <seraphine_ene@yahoo.com>
2018-12-03 09:24:56 +0000

    removed the comment

--- ch03/ch3_seraphine.py
+++ ch03/ch3_seraphine.py
@@ -1,56 +1,56 @@
 #first way
 
-#print('what is your name? ')
-#user_name = input().upper()
-#print('Hello {}!'.format(user_name))
+print('what is your name? ')
+user_name = input().upper()
+print('Hello {}!'.format(user_name))
 
 
 #second way
 
-#user_name = input('What is your name? ')
-#user_country = input('Where are you from? ')
-#
-#user_name = user_name.upper()
-#
-#print('Hello {}! you are from {}'.format(user_name,user_country.title()))
+user_name = input('What is your name? ')
+user_country = input('Where are you from? ')
+
+user_name = user_name.upper()
+
+print('Hello {}! you are from {}'.format(user_name,user_country.title()))
 
 #third way 
-#name = input("What’s your name? ")
-#city = input("What’s your city? ")
-#age = input("What’s your age? ")
-#age = str(age)
-#print ("Hello {}! from {} I am {} years old!".format(name.upper(),city.title(),age ))
+name = input("What’s your name? ")
+city = input("What’s your city? ")
+age = input("What’s your age? ")
+age = str(age)
+print ("Hello {}! from {} I am {} years old!".format(name.upper(),city.title(),age ))
 
 
-#def hello_world():
-#    my_details()
-#
-#def my_details():
-#    user_name = input('What is our name? ')
-#    print ("My name is {}".format(user_name.title()))
-#    add_numbers()
-#    
-#def add_numbers():
-#    user_age = input('what is your age ? ')
-#    print(int(user_age) + 2)
-#    favourite_player()
-#    
-#def favourite_player():
-#    input('who is your favourite player? ')
-#    print("You lie !! Your favourite player is Murray ;yeah yeah")
-#    
-#    
-#    
-#def my_calculator ():
-#    first_number = input('Enter your first number? ')
-#    second_number = input('Enter another number? ')
-#
-#   
-#    print (int(first_number) + int(second_number))
-#   
-#    
-#def hello_name(name):
-#   print('hello ' + name + '!')
+def hello_world():
+    my_details()
+
+def my_details():
+    user_name = input('What is our name? ')
+    print ("My       name{}".format(user_name.title()))
+    add_numbers()
+    
+def add_numbers():
+    user_age = input('what is your age ? ')
+    print(int(user_age) + 2)
+    favourite_player()
+    
+def favourite_player():
+    input('who is your favourite player? ')
+    print("You lie !! Your favourite player is Murray ;yeah yeah")
+    
+  
+   
+def my_calculator ():
+    first_number = input('Enter your first number? ')
+    second_number = input('Enter another number? ')
+
+ 
+    print (int(first_number) + int(second_number))
+  
+    
+def hello_name(name):
+   print('hello ' + name + '!')
 
 car = 100
 space_in_a_car = 4.0
