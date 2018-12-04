23cb0e64eec9347587de79c56911542b5293f7e1
Seraphine Young <seraphine_ene@yahoo.com>
2018-12-04 17:26:57 +0000

    working on conditionals on python

---
+++ ch04/ch04_seraphine.py
@@ -0,0 +1,22 @@
+age =15
+myage = age >=13 and age <=19
+
+print(myage)
+
+age = 22
+isaTeen = age >=13 and age <=19
+
+print(isaTeen)
+
+number = input('Enter a number 1 an 10 :')
+number = int(number)
+             
+if number > 10:
+    print('To high!')
+else:
+    print('your number is 10 or lower')
+
+
+if number <= 0:
+    print('Too low!')
+                 
