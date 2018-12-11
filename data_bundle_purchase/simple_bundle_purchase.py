0d360d57e269386212447ba77e09adbc9fa788c3
Seraphine Young <seraphine_ene@yahoo.com>
2018-12-11 17:53:58 +0000

    Added functionality of checking password 3 times

--- data_bundle_purchase/simple_bundle_purchase.py
+++ data_bundle_purchase/simple_bundle_purchase.py
@@ -1,44 +1,65 @@
 
-
-
+def userInput ():
+    attempt = input('Please enter your password?:' )
+    return attempt
 
 def DataBundlePurchase(truePasscode, balance):
     
-    if passwordCheck(truePasscode):
-        if checkBalance(balance):
-             return transaction_type(balance)     
-        else:
-            return f'Not enough balance'     
-    else: 
-        return f'Wrong passcode'
+     counter = 0
+ 
+     if userInput() == truePasscode and counter < 3:
+        return transaction_type(balance)
     
-def passwordCheck(truePasscode):
-    
-    UserInput = input('Please enter your password?:' )
-    if truePasscode == UserInput:
-        return True
-    else:
-        return False
-    
-    
+     elif userInput() != truePasscode and counter < 3:
+         counter +=1
+         userInput()
+         
+         if counter <= 3:
+             return 'you have tried 3 time, we would log you out now'
+         
+ 
+
+#def DataBundlePurchase(truePasscode, balance):
+#
+#    count = 0
+#    while count < 3:
+#        
+#        UserInput = input('Please enter your password?:' )
+#        if truePasscode == UserInput:
+#            return transaction_type(balance) 
+#      
+#        else:
+#            print('Wrong passcode')
+#            count +=1
+#            
+#            if count == 3:
+#                return'count is greater than 3'
+#        
+#
+#result = DataBundlePurchase('1234', 34.55)
+#print (result)
+       
+      
 def checkBalance(balance):
     if balance > 0:
         return True
     else:
         return False
     
-def transaction_type(balance):
-    
-    userOption = int(input('Enter 1 to check Credit Balance OR 2 to check Purchase Data bundle'))
-    
+
+
+def transaction_type(balance):  
+    userOption = int(input('Enter 1 to check Credit Balance OR 2 to check Purchase Data bundle ? '))
     if userOption == 1:
         return f'Your Credit Balance is {balance}'
-    
     elif userOption == 2:
-        return f'Purchase Data Bundle is not available at this time'
-    
+        return 'Purchase Data Bundle is not available at this time'
     else:
-        return f'Please choose a valid option'
+        return 'Please choose a valid option'
+    
+    
+
+
         
     
     
--- data_bundle_purchase/test_databundlepurchase.py
+++ data_bundle_purchase/test_databundlepurchase.py
@@ -7,15 +7,15 @@ from simple_bundle_purchase import DataBundlePurchase
 print ('TEST EXAMPLE 1')
 # database input, you will still need to check user pin
 result = DataBundlePurchase('1234', 34.55)
-print ('-----\nRESULT:', result)
-print ('-' * 50, '\n')
+print (result)
 
-print ('TEST EXAMPLE 2')
-result = DataBundlePurchase('2345', -22.00)
-print ('-----\nRESULT:', result)
-print ('-' * 50, '\n')
 
-print ('TEST EXAMPLE 3')
-result = DataBundlePurchase('3456', 17.55)
-print ('-----\nRESULT:', result)
-print ('-' * 50, '\n')
+#print ('TEST EXAMPLE 2')
+#result = DataBundlePurchase('2345', -22.00)
+#print ('-----\nRESULT:', result)
+#print ('-' * 50, '\n')
+#
+#print ('TEST EXAMPLE 3')
+#result = DataBundlePurchase('3456', 17.55)
+#print ('-----\nRESULT:', result)
+#print ('-' * 50, '\n')
