8c3995b2bc45f6d717248d86070c59cf7857d00a
Seraphine Young <seraphine_ene@yahoo.com>
2018-12-18 16:54:06 +0000

    Worked on more comments for the quit

--- ch11-While_Loop/random_dice_game.py
+++ ch11-While_Loop/random_dice_game.py
@@ -2,7 +2,7 @@
 from random import randint
 
 def randomGame ():
-    
+    game = 0
     
     userInput = ''
     
@@ -10,23 +10,24 @@ def randomGame ():
         num1 = randint(1,6)
         num2 = randint(2,6)
         add_num = num1 + num2
-        print(add_num)
+#        print(add_num)
+        game += 1
         
-        userInput = input('Enter even or odd to play or quit to exit the game')
+        userInput = input('Enter even or odd and to play or quit to exit the game ?\n' )
         if userInput == 'even' and add_num % 2 == 0:
             print(add_num)
             print('You Win Yay')
             
         elif userInput == 'odd' and add_num % 2 != 0:
-            print('You Win Yay')
+            print('You Win Yay....play again')
             
         elif userInput == 'quit':
             break
         
         else:
-            print('You lose')
+            print('You lose....play again')
             
-    print ('So sad to see you go')       
+    print (f'You have played {game} times only play 3 more times to have a perfect score...So sad to see you go')       
        
     
 
