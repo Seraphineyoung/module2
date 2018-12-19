9d26b5741560b89518e5a27b45c02ea6b3a8b39c
Seraphine Young <seraphine_ene@yahoo.com>
2018-12-19 09:23:18 +0000

    Working on Ch01 folder

---
+++ ch01 - Introduction to python/ch1_seraphine.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#In Python everything is an object...
+
+#printing hello world in Python
+
+print('Hello World')
+
+#Working on simple operations in Python
+G = 2 + 3, 5,4,6
+B = 2.9 + 3.2
+C =(3.2 / 2.0) + 7
+D = 2**3
+E = 4**(1/2)
+
+#String casting and concatination
+F = str(6) + 'a'
+A =[1,2,3,4,5]
+J = float(2.5)
+P = int(J)
+O = str(P)
+S = "my name is seraphine"
+print(S)
+print(A,'\n',B,'\n',C, '\n',D,'\n',E,'\n', F)
+V = "my name is Seraphine \n "
+M = "my name is Pamela" + "\n" + V
+print(M)
+
+#This is me learning how to comment on python or crtl A
+
+
