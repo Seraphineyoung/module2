a7ad6ffbfe538c0c4ec79ecaf75bce1cd0f30ae5
Seraphine Young <seraphine_ene@yahoo.com>
2018-12-20 09:58:20 +0000

    Working on for loops with strings

---
+++ ch12 - The_For_Loop/ch12_seraphine.py
@@ -0,0 +1,69 @@
+
+####################Task 1 ########################################
+#Loop through a list
+my_shopping_cart = ['cake','plates','plastic forks','juice','cups']
+
+for item in my_shopping_cart:
+    print(item)
+    
+#####################Task 2 #####################################
+#Update List Values
+values = [875,23,451]
+
+for val in values:
+    print('--->' + str(val))
+    
+print()   
+ 
+for val in values:    
+    print('--->' + str(val + 50))
+    
+
+#############################Task 3 ##################################
+    
+    
+    
+    
+    
+    
+    
+    
+###########################Task 4######################################
+#Loop through  a string data type
+    
+for char in "Yes":
+    print(char)
+    
+# looping through a string, you have to split the string first and then loop
+for char in 'My name is seraphine'.split():
+    print(char)
+    
+# This loops and where it see the letter e, it splits and forms a string without the letter e    
+for char in 'My name is seraphine'.split('e'):
+    print(char)   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
