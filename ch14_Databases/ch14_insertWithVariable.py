0120cc71bf4ccd94911b0768613b835f766706de
Seraphine Young <seraphine_ene@yahoo.com>
2019-01-09 10:47:13 +0000

    Added dynamic data to the table

--- ch14_Databases/ch14_insertWithVariable.py
+++ ch14_Databases/ch14_insertWithVariable.py
@@ -3,6 +3,9 @@
 #Create a table and insert data
 
 import sqlite3
+import time
+import datetime
+import random
 
 conn = sqlite3.connect('task1.db')
 c = conn.cursor()
@@ -11,15 +14,31 @@ def create_table():
     
     c.execute('CREATE TABLE IF NOT EXISTS stuffToBuild(unix REAL , datestamp TEXT, keyword TEXT, value REAL)')
     
-def data_entry():
-    c.execute("INSERT INTO stuffToBuild VALUES(142233222,'2018-01-01','python',5)" )
-        
-    conn.commit()
-    c.close()
-    conn.close()
+#def data_entry():
+#    c.execute("INSERT INTO stuffToBuild VALUES(142233222,'2018-01-01','python',5)" )
+#data_entry() 
     
-create_table()
-data_entry()
+    
+def dynamic_data_entry():
+    unix = time.time()
+    date = str(datetime.datetime.fromtimestamp(unix).strftime('%y-%m-%d %H:%M:%S'))
+    keyword = 'python'
+    value = random.randrange(0,10)
+    c.execute('INSERT INTO stuffToBuild( unix,datestamp,keyword,value) VALUES(?,?,?,?)', (unix,date,keyword,value))
+    
+    conn.commit()
+    
+create_table()    
+        
+for i in range(10):
+    dynamic_data_entry()
+    time.sleep(1)
+    
+c.close()
+conn.close()
+    
+
+
 
 
 
